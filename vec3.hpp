9054e3102796f0c21a10764c36cbb9f02dfd2aa2
Kenn <kennethassogba@gmail.com>
2022-03-27 14:04:25 +0200

    vec3 utility funcitons

--- vec3.hpp
+++ vec3.hpp
@@ -41,8 +41,8 @@ public:
     return *this *= (1 / t);
   }
 
-  double lenght() const { return sqrt(lenght_squared()); }
-  double lenght_squared() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
+  double length() const { return sqrt(length_squared()); }
+  double length_squared() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
 
 public:
   double e[3];
@@ -51,5 +51,58 @@ public:
 // Type aliases for vec3
 using point3 = vec3; // 3D point
 using color = vec3;  // RGB color
+
+// Utility functions
+inline std::ostream &operator<<(std::ostream &out, const vec3 &v)
+{
+  return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3 &u, const vec3 &v)
+{
+  return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v)
+{
+  return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &u, const vec3 &v)
+{
+  return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3 &v)
+{
+  return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v, double t)
+{
+  return t * v;
+}
+
+inline vec3 operator/(vec3 v, double t)
+{
+  return (1 / t) * v;
+}
+
+inline double dot(const vec3 &u, const vec3 &v)
+{
+  return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3 &u, const vec3 &v)
+{
+  return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+              u.e[2] * v.e[0] - u.e[0] * v.e[2],
+              u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(vec3 v)
+{
+  return v / v.length();
+}
 
 #endif
